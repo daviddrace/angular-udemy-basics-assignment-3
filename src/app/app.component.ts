1238985e54aad5d30f79cdb5619fadd9d268d881
David Drace <david@drace.net>
2017-10-30 11:14:08 -0500

    Completed assignment.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  showDetails = false;
+  clickLog = [];
+  clickCount = 0;
+
+
+  onButtonClick() {
+    this.showDetails = !this.showDetails;
+
+    // increment the number of clicks
+    this.clickCount++;
+
+    // Get the current time for the datestamp
+    const now = new Date(Date.now());
+
+    // Convert timestamp to human-readable string
+    const timeStamp = now.toUTCString();
+
+    // Add click count and timestamp to the log array
+    this.clickLog.push({
+      count: this.clickCount,
+      time: timeStamp
+    });
+
+  }
 }
